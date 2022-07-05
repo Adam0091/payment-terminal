fe0852b91c35ff05209d9f82624274fa608b94a7
Adam <adam.holo@mail.ru>
2022-07-05 15:56:09 +0400

    fix: remove unnecessary imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from "next/app";
-import styled, { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { normalize } from "styled-normalize";
 
 const Global = createGlobalStyle`
+  box-sizing: border-box;
   ${reset};
   ${normalize};
 `;
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
